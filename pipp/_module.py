01e23484d3cfef558b19c2478c1094b9aac00c7f
soroorh <soroorh@student.unimelb.edu.au>
2023-06-19 01:28:13 +1000

    add modules

---
+++ pipp/_module.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+
+    def __init__(self, input_dim=8, hidden_dim=64, latent_dim=10):
+        super().__init__()
+        self.encoder = nn.Sequential(
+                    nn.Linear(input_dim, hidden_dim),
+                    nn.ReLU()
+            )
+
+        self.mean_encoder = nn.Linear(hidden_dim, latent_dim)
+        self.var_encoder = nn.Linear(hidden_dim, latent_dim)
+
+
+    def forward(self, x):
+        # Simple forward
+        hidden = self.encoder(x)
+        mu = self.mean_encoder(hidden)
+        logvar = self.var_encoder(hidden)
+        std = logvar
+        eps = torch.randn_like(std)
+        x_sample = mu
+        return x_sample
